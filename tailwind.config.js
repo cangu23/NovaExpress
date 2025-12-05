9cc6793fe4131005079ea9ffb1aa9cb6368e41e7
CanguVT <162073871+cangu23@users.noreply.github.com>
2025-12-05 11:56:47 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,26 +2,13 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js, {js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        "in": "in 0.6s ease-out",
-        "fade-in": "fadeIn 0.6s ease-out",
-      },
-      keyframes: {
-        in: {
-          "0%": { opacity: "0", transform: "translateY(20px)" },
-          "100%": { opacity: "1", transform: "translateY(0)" },
-        },
-        fadeIn: {
-          "0%": { opacity: "0" },
-          "100%": { opacity: "1" },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [require("tailwindcss-animate")],
 }
